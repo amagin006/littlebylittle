b03d5ff37a7315606da4b880d5d276b79c6ca7b4
岩本 昭太 <amaginqoo@gmail.com>
2018-03-03 22:29:18 +0900

    gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
-
+var notify  = require('gulp-notify');
 
 gulp.task('sass', function(){
           return gulp.src('./sass/**/*.scss')
-            .pipe(plumber()) 
+            .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>") //<-
+             }))
             .pipe(sass({outputStyle: 'expanded'}))
             .pipe(gulp.dest('./css'));
           });
