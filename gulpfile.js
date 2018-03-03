069604941e80ffa18b8078314ee1633064b10493
岩本 昭太 <amaginqoo@gmail.com>
2018-03-03 22:22:10 +0900

    gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
 
 gulp.task('sass', function(){
           return gulp.src('./sass/**/*.scss')
+            .pipe(plumber()) 
             .pipe(sass({outputStyle: 'expanded'}))
             .pipe(gulp.dest('./css'));
           });
